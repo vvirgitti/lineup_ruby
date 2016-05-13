05a1774b2d5d1e29d614a54a1c1f6cb4b3ec10bc
Vanessa <virgitti@gmail.com>
2016-05-13 11:12:30 +0100

    Changed slightly titles of the tests and added a readme

---
+++ README.md
@@ -0,0 +1,8 @@
+A simple app to help create a softball (slowpitch) lineup.
+
+##Technologies used
+
+Ruby
+
+For testing:
+Rspec
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -2,22 +2,22 @@ require './src/player'
 
 describe Player do
 
-  it 'can have a name' do
+  it 'has a name' do
     player = Player.new('James', 'Pitch', 2)
     expect(player.name).to eq 'James'
   end
 
-  it 'can have a position' do
+  it 'has a position' do
     player = Player.new('Bob', 'Catch', 1)
     expect(player.position).to eq 'Catch'
   end
 
-  it 'can have a rating' do
+  it 'has a rating' do
     player = Player.new('Rob', 'Outfield', 4)
     expect(player.rating).to eq 4
   end
 
-  it 'can have a rating for a specific position' do
+  it 'has a rating for a specific position' do
     player = Player.new('Isabelle', 'First', 3)
     expect(player.positions[0]).to eq ({'position'=> 'First', 'rating' => 3})
   end
